0b9e32633ac86a3a75a3abf3f9ddc6a7bf9bf589
xiachaolun <sasnzy13@163.com>
2013-02-27 11:16:56 +0800

    1

--- distributed_gp/utility/event_feature.py
+++ distributed_gp/utility/event_feature.py
@@ -127,16 +127,10 @@ class EventFeature(Event):
 		avg_photo_dis = self.getAvgPhotoDis()
 		avg_photo_dis_cap = self.getAvgPhotoDisByCaption()
 		cap_per = self.getCaptionPercentage()
-#		people_num = self.getActualValueByCounting()
-#		duration = self.getDuration()
-#		people_num_unit = self.getActualValue() * 1.0 * 60 * 15 / self.getDuration()
-#		stop_word_per = self.getPercentageOfStopwordsFromTopWords()
 		std = self.getPredictedStd()
 		top_word_pop = self.getTopWordPopularity(k_topwords)
 		zscore = self.getZscore()
 		entropy = self.getEntropy(entropy_para)
-		# this value fixes to 1
-#		ratio = self.getRatioOfPeopleToPhoto()
 		
 		label = int(self.getLabel())
 		event_id = str(self._event['_id'])
@@ -149,18 +143,17 @@ class EventFeature(Event):
 		
 		
 #		historic_features = [0]*3  # for test only
-		historic_features = self.getHistoricFeatures(entropy_para)
-		diff_avg_photo_dis = avg_photo_dis - historic_features[0]
-		diff_top_word_pop = historic_features[1]
-		diff_entropy = historic_features[2]
+#		historic_features = self.getHistoricFeatures(entropy_para)
+#		diff_avg_photo_dis = avg_photo_dis - historic_features[0]
+#		diff_top_word_pop = historic_features[1]
+#		diff_entropy = historic_features[2]
 		
 		location_name_similarity = self.getTopPhotosLocationSimilarity()
 		location_name_same = self.checkIfTopPhotoLocationSame()
 		
-		return [avg_cap_len, avg_photo_dis, avg_photo_dis_cap, cap_per, #people_num, #duration,
-#						people_num_unit,
+		return [avg_cap_len, avg_photo_dis, avg_photo_dis_cap, cap_per,
 		        std, top_word_pop, zscore, entropy, #ratio,
-		        diff_avg_photo_dis, diff_top_word_pop, diff_entropy,
+#		        diff_avg_photo_dis, diff_top_word_pop, diff_entropy,
 		        tfidf_top3[0], tfidf_top3[1], tfidf_top3[2], 
 		        hashtage_cnt3[0], hashtage_cnt3[1], hashtage_cnt3[2],
 		        number_photos_associated_with_keywords3[0], number_photos_associated_with_keywords3[1], number_photos_associated_with_keywords3[2],
@@ -183,18 +176,13 @@ class EventFeature(Event):
 		print '@attribute AvgPhotoDis real'
 		print '@attribute AvgPhotoDisbyCap real'
 		print '@attribute CaptionPercentage real'
-		print '@attribute NumberOfPeopleInUnitTime real'
-#		print '@attribute PeopleNumber real'
-#		print '@attribute Duration real'
-#		print '@attribute PercentageOfStopwordsFromTopWords real'
 		print '@attribute PredictedStd real'
 		print '@attribute TopWordPopularity real'
 		print '@attribute Zscore real'
 		print '@attribute Entropy real'
-#		print '@attribute TheRatioOfPeopleToPhoto real'
-		print '@attribute diff_AvgPhotoDis real'
-		print '@attribute diff_TopWordPopularity real'
-		print '@attribute diff_Entropy real'
+#		print '@attribute diff_AvgPhotoDis real'
+#		print '@attribute diff_TopWordPopularity real'
+#		print '@attribute diff_Entropy real'
 
 		print '@attribute tfidf1 real'	
 		print '@attribute tfidf2 real'	
