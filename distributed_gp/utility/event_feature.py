1f97e6266ac76b3fb938716df3fbf3af03e16d73
xiachaolun <sasnzy13@163.com>
2013-03-01 11:03:11 +0800

    1

--- distributed_gp/utility/event_feature.py
+++ distributed_gp/utility/event_feature.py
@@ -143,17 +143,17 @@ class EventFeature(Event):
 		
 		
 #		historic_features = [0]*3  # for test only
-#		historic_features = self.getHistoricFeatures(entropy_para)
-#		diff_avg_photo_dis = avg_photo_dis - historic_features[0]
-#		diff_top_word_pop = historic_features[1]
-#		diff_entropy = historic_features[2]
+		historic_features = self.getHistoricFeatures(entropy_para)
+		diff_avg_photo_dis = avg_photo_dis - historic_features[0]
+		diff_top_word_pop = historic_features[1]
+		diff_entropy = historic_features[2]
 		
 		location_name_similarity = self.getTopPhotosLocationSimilarity()
 		location_name_same = self.checkIfTopPhotoLocationSame()
 		
 		return [avg_cap_len, avg_photo_dis, avg_photo_dis_cap, cap_per,
 		        std, top_word_pop, zscore, entropy, #ratio,
-#		        diff_avg_photo_dis, diff_top_word_pop, diff_entropy,
+		        diff_avg_photo_dis, diff_top_word_pop, diff_entropy,
 		        tfidf_top3[0], tfidf_top3[1], tfidf_top3[2], 
 		        hashtage_cnt3[0], hashtage_cnt3[1], hashtage_cnt3[2],
 		        number_photos_associated_with_keywords3[0], number_photos_associated_with_keywords3[1], number_photos_associated_with_keywords3[2],
@@ -180,9 +180,9 @@ class EventFeature(Event):
 		print '@attribute TopWordPopularity real'
 		print '@attribute Zscore real'
 		print '@attribute Entropy real'
-#		print '@attribute diff_AvgPhotoDis real'
-#		print '@attribute diff_TopWordPopularity real'
-#		print '@attribute diff_Entropy real'
+		print '@attribute diff_AvgPhotoDis real'
+		print '@attribute diff_TopWordPopularity real'
+		print '@attribute diff_Entropy real'
 
 		print '@attribute tfidf1 real'	
 		print '@attribute tfidf2 real'	
