0c18c9088dbf002201ef4678c85c9e94ebafde74
xiachaolun <sasnzy13@163.com>
2013-03-03 00:15:49 +0800

    1

--- distributed_gp/utility/test.py
+++ distributed_gp/utility/test.py
@@ -21,19 +21,16 @@ def main():
 	ei.setDB('citybeat')
 	ei.setCollection('candidate_event_25by25_merged')
 	
-	ei2 = EventInterface()
-	ei2.setDB('citybeat')
-	ei2.setCollection('online_candidate')
-	ids = ['51148288c2a3754cfe668edd', '51147952c2a3754cfe6684ee', '51148d89c2a3754cfe669eb4', '51147967c2a3754cfe668503']
-	ids += ['51147b20c2a3754cfe668697', '51148a7ec2a3754cfe669977', '51147ad8c2a3754cfe668670', '51148ba7c2a3754cfe669b75']
-
-#511477ffc2a3754cfe668450 1
-#511497d3c2a3754cfe66ac3f 1
-#5114a199c2a3754cfe66b7ba 1
-#51147c8cc2a3754cfe6687ea 1]
-	for my_id in ids:
-		event = ei.getDocument({'_id':ObjectId(my_id)})
-		ei2.addEvent(event)
+	event = ei.getEventByID('511486b7c2a3754cfe6694e2')
+	
+	e = Event(event)
+	
+	print len(event['photos'])
+	print e._getActualValueByCounting()
+	e.selectOnePhotoForOneUser()
+	ee = e.toJSON()
+	print len(ee['photos'])
+	
 	
 if __name__=='__main__':
 	main()
