d6d94f26d6530cd6d3c6f300632799a9bc43128c
xiachaolun <sasnzy13@163.com>
2013-03-21 03:33:12 +0800

    1

--- distributed_gp/utility/process_data.py
+++ distributed_gp/utility/process_data.py
@@ -193,7 +193,7 @@ def main():
 	dataEdition = sys.argv[2]
 	sparse = False
 #	sparse = sys.argv[2] == 'sparse'
-	generateData(not balanced, sparse, dataEdition)
+	generateData2(not balanced, sparse, dataEdition)
 
 if __name__=='__main__':
 	main()
