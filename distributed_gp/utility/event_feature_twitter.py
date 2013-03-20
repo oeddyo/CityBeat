56b2bfab68dd8a82fa2301d07c4168c05b4dfc0b
xiachaolun <sasnzy13@163.com>
2013-03-21 06:10:28 +0800

    1

--- distributed_gp/utility/event_feature_twitter.py
+++ distributed_gp/utility/event_feature_twitter.py
@@ -52,8 +52,8 @@ class EventFeatureTwitter(EventFeature):
 		
 		
 		
-#		historic_features = [0]*3  # for test only
-		historic_features = self.getHistoricFeatures(entropy_para)
+		historic_features = [0]*3  # for test only
+#		historic_features = self.getHistoricFeatures(entropy_para)
 		diff_avg_photo_dis = avg_photo_dis - historic_features[0]
 		diff_top_word_pop = historic_features[1]
 		diff_entropy = historic_features[2]
@@ -61,7 +61,7 @@ class EventFeatureTwitter(EventFeature):
 		location_name_similarity = self.getTopPhotosLocationSimilarity()
 #		location_name_same = self.checkIfTopPhotoLocationSame()
 
-#		twitter_features = self.extractFeatureFromTweet()
+		twitter_features = self.extractFeatureFromTweet()
 		
 		return [avg_cap_len,
 		        min_photo_dis, max_photo_dis, std_photo_dis, avg_photo_dis, median_photo_dis,
@@ -76,7 +76,7 @@ class EventFeatureTwitter(EventFeature):
 		        location_name_similarity, 
 #		        location_name_same,
 #						twitter_features,
-#						twitter_features[0], twitter_features[1],
+						twitter_features[0], twitter_features[1],
 		        event_id,
 		        label]
 		        
@@ -126,8 +126,8 @@ class EventFeatureTwitter(EventFeature):
 		print '@attribute Top10PhotoLocationNameFreq real'
 #		print '@attribute Top3PhotoLocationNameSame real'
 
-#		print '@attribute PercentageOfTweetsWithTopWords real'
-#		print '@attribute diff_PercentageOfTweetsWithTopWords real'
+		print '@attribute PercentageOfTweetsWithTopWords real'
+		print '@attribute diff_PercentageOfTweetsWithTopWords real'
 								
 		print '@attribute ID string'
 		print '@attribute label {1,-1}'
@@ -145,9 +145,9 @@ class EventFeatureTwitter(EventFeature):
 			keywords.append(word)
 			
 		per = tc.computePercentageOfTweetWithKeyword(keywords, 1)
-		return per
-#		diff_per = tc.computeDifferenceComparedWithHistoricPercentageOfTweetWithKeyword(keywords, 1)
-#		return per, diff_per
+#		return per
+		diff_per = tc.computeDifferenceComparedWithHistoricPercentageOfTweetWithKeyword(keywords, 1)
+		return per, diff_per
 
 if __name__=='__main__':
 	generateData()
