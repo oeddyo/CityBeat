b5bd11f3a288f9eaa89cde850814edf291e69a36
xiachaolun <sasnzy13@163.com>
2013-03-21 04:12:44 +0800

    1

--- distributed_gp/utility/tweet_cluster.py
+++ distributed_gp/utility/tweet_cluster.py
@@ -30,10 +30,12 @@ class TweetCluster(object):
 		
 	def setTweetClusterRegion(self, region):
 		self._tweet_cluster['region'] = region
+		print region
 		
 	def setPeriod(self, period):
 		# period should be [begin_time, end time]
 		self._tweet_cluster['period'] = period
+		print period
 		
 	def getTweetFromRangeQuery(self):
 		ti = TweetInterface()
